b401f53e0dc4b304350c9654fc1f7085c6566284
rachelweisenburger <rachelweisenburger@utexas.edu>
2025-07-17 15:15:56 -0500

    back to the beginning

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const path = require('path');
 var app = express();
 
 // --- Static website directories
-app.use(express.static('views'));
+app.use(express.static(__dirname + ''));
 app.use('/jsPsych', express.static(__dirname + "/jsPsych"));
-app.set('views', path.join(__dirname + ''));
+app.set('views', __dirname + '');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
@@ -24,16 +24,17 @@ var urlencodedparser = body_parser.urlencoded({extended:true})
 
 // --- ROUTING
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'sret-self.html'));
+    response.render('sret.html');
 });
 
 app.get('/experiment', function(request, response) {
-    response.render('sret-self.html');
+    response.render('sret.html');
 });
+
 
 // --- Tell the data where to go
 app.post('/experiment-data', urlencodedparser, function(request, response) {
-    console.log('Made it to app.post!');
+    // console.log('app.post-ing the data');
     class Sret extends Model {}
     Sret.init({
       data: DataTypes.JSON
@@ -46,7 +47,7 @@ app.post('/experiment-data', urlencodedparser, function(request, response) {
       const data = await Sret.create({
         data: request.body
       });
-      console.log(data);
+      //console.log(data);
 })();
     response.redirect('/');
 });
@@ -58,6 +59,6 @@ if (port == null || port == "") {
 }
 
 var server = app.listen(port, function(){
-    console.log('Listening on port %d', server.address().port);
 });
+
 
